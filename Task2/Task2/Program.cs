2e34b5c88d2ae45c46ff4af1a2dd011ea03ff8d6
AlderFrond <AlderFrond@yandex.ru>
2014-10-14 20:49:25 +0400

    Квадратное уравнение

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -10,6 +10,52 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            int a; int b; int c; int d; 
+            Console.WriteLine("Введите а");
+           
+            a = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите b");
+            b = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите c");
+            c = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("{0}*x^2 + {1}*x + {2} = 0", a, b, c);
+
+            d = b*b - 4*a*c;
+            Console.WriteLine("d = {1}^2 - 4*{0}*{2} = {3}",a ,b ,c, d);
+            if (d < 0)
+            {
+                Console.WriteLine("d < 0, Корней нет");
+            }
+            else if (d == 0)
+            {
+                Console.WriteLine("d = 0, Один корень");
+                double x1 = -b / (2 * a);
+                Console.WriteLine("x = -{1} / 2*{0} = {3}", a, b, d, x1);
+            }
+            else
+            {
+                Console.WriteLine("d > 0, Два кореня");
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("x1 = (-{1} + {2})/ 2*{0} = {3}", a, b, d, x1);
+                Console.WriteLine("x2 = (-{1} - {2})/ 2*{0} = {3}", a, b, d, x2);
+            }
+
+            Console.ReadKey();
+            //ax^2 + bx + c = 0
+                //d = b*b-4ac
+
+
+
+
+
+
+
+
+            /*
             int WeightByStr = 20;
 
 
@@ -36,6 +82,7 @@ namespace Task2
             }
                 
             Console.ReadKey();
+             */
         }
     }
 }
